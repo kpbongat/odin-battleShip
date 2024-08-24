77a215477d5ed6458fa56792fc7091e5ef7e4bcd
kpbongat <kpbongat.dev@gmail.com>
2024-08-24 17:35:11 +0800

    Add placeShips function to initialize boards

--- src/index.js
+++ src/index.js
@@ -4,13 +4,39 @@ import DOM from "./DOM";
 import Ship from "./Ship.js";
 import GameController from "./GameController.js";
 const playerOne = new Player();
-const shipOne = new Ship(1);
-const shipTwo = new Ship(4);
-playerOne.gameboard.place(shipOne, [1, 1]);
-playerOne.gameboard.place(shipTwo, [5, 2]);
-playerOne.gameboard.place(shipTwo, [5, 3]);
-playerOne.gameboard.place(shipTwo, [5, 4]);
-playerOne.gameboard.place(shipTwo, [5, 5]);
+placeShips(playerOne, 1);
 DOM.initializeBoard(playerOne, 1);
 const playerTwo = new Player();
+
+placeShips(playerTwo, 2);
 DOM.initializeBoard(playerTwo, 2);
+function placeShips(player, id) {
+  const ships = [
+    new Ship(1),
+    new Ship(1),
+    new Ship(1),
+    new Ship(1),
+    new Ship(2),
+    new Ship(2),
+    new Ship(2),
+    new Ship(3),
+    new Ship(3),
+    new Ship(4),
+  ];
+  for (let ship of ships) {
+    for (let i = 0; i < ship.length; i++) {
+      let x;
+      let y;
+      if (id === 2) {
+        [x, y] = GameController.getRandomMove();
+      } else {
+        const coords = prompt(
+          `Placing ship length ${ship.length} ${i + 1}/${ship.length}`
+        );
+        [x, y] = coords.split(",");
+      }
+
+      player.gameboard.place(ship, [x, y]);
+    }
+  }
+}
